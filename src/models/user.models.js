e866ea2cfeeb3d6b7fd2d3b13c3d1859744e4593
Vipul-sh01 <vipulsharma6021@gmail.com>
2024-08-01 17:03:27 +0530

    add auth

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -47,4 +47,27 @@ userSchema.pre("save",async function(next){
 userSchema.methods.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password, this.password);
 };
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        username: this.username,
+        email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+)
+};
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this_id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+)
+}
 export const User = mongoose.model("User", userSchema);
