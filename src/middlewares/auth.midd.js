fbf1a2bf454025c9d830f8ab038b2551bcfe5bb6
Vipul-sh01 <vipulsharma6021@gmail.com>
2024-08-12 18:32:46 +0530

    add logOut

---
+++ src/middlewares/auth.midd.js
@@ -0,0 +1,23 @@
+import { User } from "../models/user.models";
+import { ApiError } from "../utility/ApiError";
+import { asyncHandler } from "../utility/asyncHandler";
+
+export const verifyJWT = asyncHandler(async(req, _, next) =>{
+    try {
+        const token = req.cookie?.accessToken || req.header("Authorization")?.replace("Bearer ", " ");
+    
+        if (!token) {
+            throw new ApiError(401, "Unauthorised requast");
+        }
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = User.findById(decodedToken?._id).select("-password -refreshToken");
+    
+        if (!user) {
+            throw new ApiError(401, "Invalid Acceses Token");
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(402, error?.message || "Invalid Acceses Token");
+    }
+});
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, loggOutUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.midd.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
+router.route("/logIn").post(loginUser);
+// for logOut loggic
+router.route("/logOut").post(verifyJWT, loggOutUser)
 
 export default router;
