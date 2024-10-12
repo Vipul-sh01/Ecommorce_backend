3a7045231271f6ac3dd9689d6fc254f1a1038478
Vipul sharma <vipulsharma6021@gmail.com>
2024-10-13 00:44:47 +0530

    Bug fix

--- src/utility/asynchandler.js
+++ src/utility/asynchandler.js
@@ -7,4 +7,20 @@ const asyncHandler = (requestHandler) => {
     };
 };
 
+// const asyncHandler = (fn) => {
+//     return async (req, res, next) => {
+//         try {
+//             await fn(req, res, next);
+//         } catch (error) {
+//             res.status(error.status || 500).json({
+//                 success: false,
+//                 message: error.message,
+//             });
+//         }
+//     };
+// };
+
+
+
+
 export { asyncHandler};
